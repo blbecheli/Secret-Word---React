bbf6a9750f240e5666044e483ddb2bc3d818791d
blbecheli <blbecheli@gmail.com>
2023-09-04 09:30:35 -0700

    My first project React - Secret Word

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,16 @@
+import styles from './StartScreen.module.css'
+
+// eslint-disable-next-line react/prop-types
+const StartScreen = ({startGame}) => {
+    return (
+        <>
+            <div className={styles.start}>
+            <h1>Secret Word</h1>
+            <p>Clique no botão abaixo para começar a jogar</p>
+            <button onClick={startGame}>Começar o jogo</button>
+            </div>
+        </>
+    )
+}
+
+export default StartScreen
